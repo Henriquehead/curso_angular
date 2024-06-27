56f38bcf44036b7e5e0e0085e8cfbc805d2c2660
Henriquehead <carloshenriquegastronomia@gmail.com>
2024-06-26 22:33:22 -0300

    "ng-content"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { UsersComponent } from './pages/users/users.component';
 
 
 
@@ -14,7 +16,9 @@ import { FooterComponent } from './_components/footer/footer.component';
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    BaseUiComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
